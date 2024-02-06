4570f2eb81092fcf260dcd372c6860bfbf020f8b
davidespinosa8 <ddavidespinosa8@gmail.com>
2024-02-06 11:48:13 -0300

    modificaciones de link

--- src/components/Header/dataHeader.tsx
+++ src/components/Header/dataHeader.tsx
@@ -1,20 +1,21 @@
-import { RiInstagramLine, RiYoutubeLine, RiLinkedinBoxLine, RiTwitterXFill, } from "react-icons/ri";
+import { RiInstagramLine, RiLinkedinBoxLine, RiGithubLine, } from "react-icons/ri";
+import { SiCanva } from "react-icons/si";
 
 export const socialNetworks = [
   {
     logo: <RiInstagramLine size="40" />,
-    src: "https://www.instagram.com/davidespinosa8",
+    src: "https://www.instagram.com/legiondigital__/",
   },
   {
-    logo: <RiYoutubeLine size="40" />,
-    src: "https://www.youtube.com/@dariodavidespinosa9861",
+    logo: <RiGithubLine size="40" />,
+    src: "https://github.com/Davidespinosa8",
   },
   {
     logo: <RiLinkedinBoxLine size="40"/>,
     src: "https://www.linkedin.com/in/dar%C3%ADo-david-espinosa-b50972258",
   },
-  {
-    logo: <RiTwitterXFill size="40" />,
-    src: "https://twitter.com/S1r1uS_087",
+    {
+    logo: <SiCanva size="40"/>,
+    src: "https://davidespinosa8.my.canva.site/cv-david-espinosa",
   },
 ];
